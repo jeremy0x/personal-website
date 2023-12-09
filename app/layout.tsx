f4d6fe99f2131abd04a23ef60017ec5e9e136882
Jeremiah Aworetan <aworetanjeremiah@gmail.com>
2023-12-09 08:31:42 +0100

    feat: migrate to new vercel speed insights package
    Vercel updated how their speed insights works, so the need for this update.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -49,6 +50,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
